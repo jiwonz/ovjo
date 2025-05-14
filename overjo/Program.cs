94a44fdc47ea8ed12448ee35e51755e368a5a424
jiwonz <jiwonz0113@gmail.com>
2025-05-14 17:46:29 +0900

    Implemenet OpenSandbox

--- overjo/Program.cs
+++ overjo/Program.cs
@@ -1,6 +1,17 @@
 ﻿using RobloxFiles;
+using Spectre.Console;
 using System.CommandLine;
+using System.Diagnostics;
 using System.Reflection;
+using System.Text.Json;
+
+const string SANDBOX_APP_NAME = "20687893280c48c787633578d3e0ca2e";
+const string ERROR_PREFIX = "[bold red]error[/]:";
+
+var stderrConsole = AnsiConsole.Create(new AnsiConsoleSettings
+{
+    Out = new AnsiConsoleOutput(Console.Error)
+});
 
 T? CreateInstance<T>(string className) where T : class
 {
@@ -24,20 +35,95 @@ T? CreateInstance<T>(string className) where T : class
     return null;
 }
 
+Process StartProcess(string command, string args = "")
+{
+    Process p = new Process();
+    p.StartInfo = new ProcessStartInfo(command, args);
+    p.StartInfo.RedirectStandardOutput = false;
+    p.StartInfo.RedirectStandardError = true;
+    p.StartInfo.UseShellExecute = false;
+    p.StartInfo.CreateNoWindow = true;
+    p.Start();
+
+    return p;
+}
+
+void OpenSandbox()
+{
+    string programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+
+    string manifestsPath = Path.Combine(programDataPath, "Epic", "EpicGamesLauncher", "Data", "Manifests");
+
+    if (!Directory.Exists(manifestsPath))
+    {
+        stderrConsole.MarkupLine($"{ERROR_PREFIX} Manifest folder does not exist.");
+        Environment.Exit(1);
+    }
+
+    string[] itemFiles = Directory.GetFiles(manifestsPath, "*.item");
+
+    foreach (string file in itemFiles)
+    {
+        Console.WriteLine($"Reading file: {file}");
+
+        string content = File.ReadAllText(file);
+
+        var manifest = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
+        if (manifest == null || manifest["AppName"].ToString() != SANDBOX_APP_NAME)
+        {
+            continue;
+        }
+        Console.WriteLine($"found OVERDARE Sandbox! {content}");
+        string? installLocation = manifest["InstallLocation"].ToString();
+        if (installLocation == null)
+        {
+            stderrConsole.MarkupLine($"{ERROR_PREFIX} Install location not found in manifest.");
+            Environment.Exit(1);
+        }
+        string? launchExecutable = manifest["LaunchExecutable"].ToString();
+        if (launchExecutable == null)
+        {
+            stderrConsole.MarkupLine($"{ERROR_PREFIX} Launch executable not found in manifest.");
+            Environment.Exit(1);
+        }
+        string programPath = Path.Combine(installLocation, launchExecutable);
+        if (!File.Exists(programPath))
+        {
+            stderrConsole.MarkupLine($"{ERROR_PREFIX} Launch executable not found.");
+            Environment.Exit(1);
+        }
+        StartProcess(programPath);
+    }
+}
+
+void BuildProject()
+{
+}
+
+void Syncback()
+{
+}
+
 Part? part = CreateInstance<Part>("Part");
 Console.WriteLine(part);
 
-var greetCommand = new Command("greet", "Greets a person");
+var editCommand = new Command("edit", "Edits rojo project");
 
-var nameArg = new Argument<string>("name", "Name of the person to greet");
-greetCommand.AddArgument(nameArg);
+var projectOpt = new Option<String>("project", "Path to the project");
+editCommand.AddOption(projectOpt);
 
-greetCommand.SetHandler((string name) =>
+editCommand.SetHandler((string project) =>
 {
-    Console.WriteLine($"Hello, {name}!");
-}, nameArg);
+    project = project ?? "default.project.json";
+    Console.WriteLine($"Hello, {project}!");
+}, projectOpt);
 
-var rootCommand = new RootCommand("My CLI");
-rootCommand.AddCommand(greetCommand);
+var rootCommand = new RootCommand("ovjo");
+rootCommand.AddCommand(editCommand);
+
+OpenSandbox();
+
+stderrConsole.MarkupLine($"{ERROR_PREFIX} bruh!");
+Environment.Exit(1);
 
 return await rootCommand.InvokeAsync(args);
